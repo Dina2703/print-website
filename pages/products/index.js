3820fb1d519d77dea2be2f367dc0a6cb27b65c4d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-28 14:09:46 -0500

    linking to a dynamic page from products

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,32 @@
-import React from "react";
+import Link from "next/link";
+
+const products = [
+  { name: "product-1" },
+  { name: "product-2" },
+  { name: "product-3" },
+  { name: "product-4" },
+];
 
 function Catalog() {
-  return <div>Products page</div>;
+  return (
+    <div>
+      <h4>Products page</h4>
+
+      {products.map((product) => {
+        return (
+          <div key={product.name}>
+            <Link
+              as={"/products/" + product.name}
+              href="/products/[product]"
+              passHref={true}
+            >
+              <button>{product.name}</button>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Catalog;
