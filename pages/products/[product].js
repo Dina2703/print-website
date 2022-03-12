cb32b52808886f9b9981d871244fd866da26b2ee
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-12 14:35:43 -0500

    Btn GoBack in Product page

--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,10 +1,17 @@
+import { Button } from "@mui/material";
 import { useRouter } from "next/router";
+import MuiNextLink from "../../components/MuiNextLink";
 
 function Product() {
   const router = useRouter();
   return (
     <div>
       This is a dynamic route for a each product info {router.query.product}
+      <MuiNextLink href="/" underline="none">
+        <Button variant="contained" sx={{ bgcolor: "#20ABFF" }} size="small">
+          Go Back
+        </Button>
+      </MuiNextLink>
     </div>
   );
 }
