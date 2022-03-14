701d02f09619f8e67976ab6bba2757dab17847c2
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-14 08:47:06 -0400

    added vercel pointend

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://versel.com/dina2703/print-website";
+export const server = dev ? "http://localhost:3000" : `https://api.vercel.com`;
